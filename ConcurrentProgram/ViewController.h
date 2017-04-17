1830fc387589fa076d4e6efb2be0cc93fa339139
lishi <lishi6@huawei.com>
2017-04-17 15:16:18 +0800

    Initial Commit

---
+++ ConcurrentProgram/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ConcurrentProgram
+//
+//  Created by yanliu on 17/04/2017.
+//  Copyright © 2017 huawei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ConcurrentProgram/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ConcurrentProgram
+//
+//  Created by yanliu on 17/04/2017.
+//  Copyright © 2017 huawei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
